a2e37f459b0150de95fc2a51bc9448e3102648ea
LevyAM <levyzudo@msn.com>
2021-04-18 16:45:25 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
+import upload from "@config/upload";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -7,8 +9,6 @@ import swaggerUi from "swagger-ui-express";
 import { AppError } from "@shared/errors/AppError";
 import "@shared/container";
 import createConnection from "@shared/infra/typeorm";
-
-import upload from "@config/upload";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
@@ -23,6 +23,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
